edfc7f7c71b8a6e550855a7ec4e283018da18625
sw30041995 <sw1225@yandex.ru>
2021-11-16 23:38:26 +0300

    add new file

---
+++ 1.js
@@ -0,0 +1,26 @@
+//1
+// Hello, my name is Ivan.
+/* I learn javascript */
+
+
+//2
+var myName;
+
+
+//3
+// Setup
+var a;
+
+// Only change code below this line
+
+a = 7;
+
+//4
+// Setup
+var a;
+a = 7;
+var b;
+
+// Only change code below this line
+
+b = a;
