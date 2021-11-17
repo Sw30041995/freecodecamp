159e014f4fff67feb52358e84461e0aeda4c8942
sw30041995 <sw1225@yandex.ru>
2021-11-17 21:40:44 +0300

    add new files

---
+++ nest-one-array-within-another-array.js
@@ -0,0 +1,2 @@
+// Only change code below this line
+const myArray = [['Ivan', 26], true];
---
+++ shopping-list.js
@@ -0,0 +1,7 @@
+const myList = [
+  ['Kia', 2015],
+  ['Toyota', 2019],
+  ['Nissan', 2013],
+  ['Opel', 2011],
+  ['Hyundai', 2009]
+];
