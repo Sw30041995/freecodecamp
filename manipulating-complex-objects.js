1e1e7d91cb625ba680f061d486c31b4677b83b59
sw30041995 <sw1225@yandex.ru>
2021-11-18 23:30:26 +0300

    add new files

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,23 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+    {
+    "artist": "Аллегрова",
+    "title": "Аллегрова",
+    "release_year": 2017,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+  },
+];
